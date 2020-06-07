65f3ad20b5383b444bbc37b9fceb36c4b33cc898
Wayne Lloyd <waynelloyd2@gmail.com>
2020-06-07 03:06:52 +0000

    Add new file

---
+++ cfg-install.sh
@@ -0,0 +1,14 @@
+git clone --bare git@gitlab.com:waynelloyd/cfg.git $HOME/.cfg
+function cfg {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .cfg-bak
+cfg checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .cfg-bak/{}
+fi;
+cfg checkout
+cfg config status.showUntrackedFiles no
